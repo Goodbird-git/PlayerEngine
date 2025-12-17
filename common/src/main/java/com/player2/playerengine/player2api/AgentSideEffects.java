23c2f34c04ab4c8bb4f69d6d7643d9cfc3ca968b
lorisj-elefant <loris@elefant.gg>
2025-12-17 12:20:40 -0800

    build

--- common/src/main/java/com/player2/playerengine/player2api/AgentSideEffects.java
+++ common/src/main/java/com/player2/playerengine/player2api/AgentSideEffects.java
@@ -85,9 +85,10 @@ public class AgentSideEffects {
         }
 
         // add quotes to build_structure so it gets proccessed as one arg:
-        String processedCommandWithPrefix = commandWithPrefix.replaceFirst(
-                "^(@build_structure)\\s+(?![\"'])(.+)$",
-                "$1 \"$2\"");
+        String processedCommandWithPrefix = commandWithPrefix;
+        // String processedCommandWithPrefix = commandWithPrefix.replaceFirst(
+        //         "^(@build_structure)\\s+(?![\"'])(.+)$",
+        //         "$1 \"$2\"");
 
         cmdExecutor.execute(processedCommandWithPrefix, () -> {
             if (mod.isStopping) {
