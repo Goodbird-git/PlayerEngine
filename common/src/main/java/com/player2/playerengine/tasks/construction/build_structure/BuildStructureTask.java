fbf44aa038ec35f42ce28e7bad82aa751b36f1c6
lorisj-elefant <loris@elefant.gg>
2025-12-23 14:58:28 -0800

    queue for struct

--- common/src/main/java/com/player2/playerengine/tasks/construction/build_structure/BuildStructureTask.java
+++ common/src/main/java/com/player2/playerengine/tasks/construction/build_structure/BuildStructureTask.java
@@ -28,6 +28,8 @@ import com.player2.playerengine.player2api.Player2APIService;
 import com.player2.playerengine.player2api.Prompts;
 import com.player2.playerengine.tasks.base.Task;
 import com.player2.playerengine.util.time.TimerGame;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -39,6 +41,7 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.properties.Property;
+import com.player2.playerengine.tasks.construction.build_structure.StructureFromCode.SetBlockCommand;
 
 public class BuildStructureTask extends Task {
     private static final int maxNumErrors = 2;
@@ -306,6 +309,8 @@ public class BuildStructureTask extends Task {
 
     private class BuildFromCode extends Task {
         String code;
+        private TimerGame blockPlaceTimer = new TimerGame(0.1f);
+        Deque<SetBlockCommand> setBlockQueue = new ArrayDeque<>();
 
         private ExecutorService buildThread;
         // outer Option: is done, inner option: is error
@@ -314,17 +319,15 @@ public class BuildStructureTask extends Task {
         public BuildFromCode(String code) {
             this.code = code;
             this.buildThread = Executors.newSingleThreadExecutor();
+            synchronized (setBlockQueue) {
+                setBlockQueue.clear();
+            }
             buildThread.submit(() -> {
                 StructureFromCode.buildStructureFromCode(code, setBlockData -> {
-                    LOGGER.info("setBlock(x={}, y={}, z={}, blockName={})",
-                            setBlockData.x, setBlockData.y, setBlockData.z, setBlockData.blockName);
-                    ResourceLocation id = new ResourceLocation("minecraft", setBlockData.blockName);
-                    Block block = BuiltInRegistries.BLOCK.get(id);
-                    // 3 means send to clients (2) and notify neighbors/update block states (1).
-                    // maybe do 2 if you dont want
-                    // redstone/etc updating/torches falling probably
-                    mod.getWorld().setBlock(new BlockPos(setBlockData.x, setBlockData.y, setBlockData.z),
-                            block.defaultBlockState(), 3);
+                    // Queue up
+                    synchronized (setBlockQueue) {
+                        setBlockQueue.add(setBlockData);
+                    }                    
                 }, (errStr) -> {
                     result = Optional.of(Optional.of(errStr));
                 }, () -> {
@@ -349,10 +352,33 @@ public class BuildStructureTask extends Task {
         protected void onStop(Task var1) {
             // TODO Auto-generated method stub
             buildThread.shutdownNow();
+            synchronized (setBlockQueue) {
+                setBlockQueue.clear();
+            }
         }
 
         @Override
         protected Task onTick() {
+
+            synchronized (setBlockQueue) {
+                if (setBlockQueue.size() == 0) {
+                    return null;
+                }
+                if (!blockPlaceTimer.elapsed()) {
+                    return null;
+                }
+                SetBlockCommand setBlockData = setBlockQueue.poll();
+                LOGGER.info("setBlock(x={}, y={}, z={}, blockName={})",
+                        setBlockData.x, setBlockData.y, setBlockData.z, setBlockData.blockName);
+                ResourceLocation id = new ResourceLocation("minecraft", setBlockData.blockName);
+                Block block = BuiltInRegistries.BLOCK.get(id);
+                // 3 means send to clients (2) and notify neighbors/update block states (1).
+                // maybe do 2 if you dont want
+                // redstone/etc updating/torches falling probably
+                mod.getWorld().setBlock(new BlockPos(setBlockData.x, setBlockData.y, setBlockData.z),
+                        block.defaultBlockState(), 3);
+                blockPlaceTimer.reset();
+            }
             // TODO Auto-generated method stub
             return null;
         }
